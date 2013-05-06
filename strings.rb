a682cd5c8599a254136c81ae3f6781658edb040d
bullockzhou <bullock0330@gmail.com>
2013-05-06 18:18:00 +0800

    add

---
+++ strings.rb
@@ -0,0 +1,24 @@
+# strings
+
+#   ruby F:\github\helloruby\strings.rb
+
+puts 12 + 12  #24
+puts '12' + '12'  #1212
+puts ' 12 + 12'   #12+12
+
+
+puts 'bling '*4  
+# bling bling bling bling
+puts 'bling *4 '
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts 'you\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
+
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
