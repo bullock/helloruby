641c03a7b21e3346c9de256694c836985575e278
bullockzhou <bullock0330@gmail.com>
2013-05-07 19:09:11 +0800

    add

---
+++ week1/calculator.rb
@@ -0,0 +1,23 @@
+#calculator
+
+# ruby F:\github\helloruby\week1\calculator.rb
+
+# puts 
+# puts "Enter your first number:"
+# num1 = gets.chomp
+# puts
+
+# puts "Enter your second number:"
+# num2 = gets.chomp
+
+# puts "What would you like to do? 1)add 2)subtract 3)divide 4)multiply"
+# command = gets.chomp
+
+
+print "how old are you?" 
+age=gets.chomp() 
+print "how tall are you" 
+height=gets.chomp() 
+print "how much do you weight?" 
+weight=gets 
+puts "so ,you're #{age} old,#{height} tall and#{weight} heavy." 
