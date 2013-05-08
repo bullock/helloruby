8f9f95cd857c936a1e5b5e10ba92517de88ca639
bullockzhou <bullock0330@gmail.com>
2013-05-08 21:41:20 +0800

    Revert "bullock"
    This reverts commit 337675fcb93f93629e7bc6d4ad4fa661291839e9.

--- strings.rb
+++ strings.rb
@@ -2,8 +2,6 @@
 
 #   ruby F:\github\helloruby\strings.rb
 
-# ruby helloruby/strings.rb
-
 puts 12 + 12  #24
 puts '12' + '12'  #1212
 puts ' 12 + 12'   #12+12
@@ -24,9 +22,3 @@ puts 'up\down'
 
 puts 'I like ' + 'apple pie.'
 puts 'I like' + ' apple pie.'
-
-# 格式化输出
-printf("%d %.2f %s\n", 1, 99.3, "Justin")
-#sprintf 格式化字符串
-x = sprintf "%d %.2f %s", 1, 99.3, "Justin"
-puts x
--- week1/calculator.rb
+++ week1/calculator.rb
@@ -38,5 +38,3 @@ puts
 print "input your name :"
 name = gets
 puts "Hello,#{name}!"
-
-print("%d %.2f %s\n", 1, 99.3, "Justin")
