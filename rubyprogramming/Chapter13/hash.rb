9c47679b86d129f8c9286fd32ff73aa1c423f768
bullockzhou <bullock0330@gmail.com>
2013-05-09 17:34:38 +0800

    RubyExercises

---
+++ rubyprogramming/Chapter13/hash.rb
@@ -0,0 +1,36 @@
+# 遍历hash
+restaurant_menu = { "Ramen" => 3, "Dal Makhani" => 4, "Coffee" => 2 }
+restaurant_menu.each do | item, price |
+  puts "#{item}: $#{price}"
+end
+
+
+
+
+cities = {'CA' => 'San Francisco',
+  'MI' => 'Detroit',
+  'FL' => 'Jacksonville'}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+def find_city(map, state)
+  if map.include? state
+    return map[state]
+  else
+    return "Not found."
+  end
+end
+
+# ok pay attention!
+cities[:find] = method(:find_city)
+
+while true
+  print "State? (ENTER to quit) "
+  state = gets.chomp
+
+  break if state.empty?
+
+  # this line is the most important ever! study!
+  puts cities[:find].call(cities, state)
+end
