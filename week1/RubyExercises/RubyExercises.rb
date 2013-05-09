61fd197085dbc4f4c2d16c8f70276270f7f22e39
Bullock <bullock0330@gmail.com>
2013-05-09 17:44:20 +0800

    Update RubyExercises.rb

--- week1/RubyExercises/RubyExercises.rb
+++ week1/RubyExercises/RubyExercises.rb
@@ -15,9 +15,11 @@ puts
 
 # 3. Now, using the same array from #2, use the "select" method to extract all odd numbers into a new array.
 p array.values_at(0,2,4,6,8)
+p array_new = array.select{|x| x%2 != 0}
+
 
 # 4. Append "11" to the end of the array. Prepend "0" to the beginning.
-array.push(11)
+array.unshift(0).push(11)
 
 # 5. Get rid of "11". And append a "3".
 array.push(11)
@@ -30,18 +32,31 @@ p array
 
 # 7. What's the major difference between an Array and a Hash?
 
+# 8. Create a Hash using both Ruby 1.8 and 1.9 syntax.
+puts 8
+puts old_hash = {:a=>1, :b => 2,:c=>3}
+puts new_hash = {a:1,b:2,c:3}
 
-h = {a:1, b:2, c:3, d:4}
+
 
 # 9. Get the value of key "b".
+puts 9
+h = {a:1, b:2, c:3, d:4}
 p h[:b]
 
 # 10. Add to this hash the key:value pair {e:5}
-h["e"] = 5    #疑问
+puts 10
+h[:e] = 5   
 p h
 
 # 13. Remove all key:value pairs whose value is less than 3.5
+puts 13
 h.delete_if{|key,value|  value <3.5}
 print h
 
-puts 
+# 14. Can hash values be arrays? Can you have an array of hashes? (give examples)
+puts 14
+hash_array = {a: [1,2,3], b:[1,2,3]}
+array_hash = [{a:1, b:2}, {c:3, d:10}]
+puts hash_array
+
